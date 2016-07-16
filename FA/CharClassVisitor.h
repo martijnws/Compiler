50f77a3edd5005be06c38fc54043875325e87b71
Martijn Wijers <martijnws@gmail.com>
2016-07-16 14:29:28 +0100

    Rename FABuilderVisitorBase to CharClassVisitor

---
+++ FA/CharClassVisitor.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "RangeKey.h"
+#include <SyntaxTreeLib/Visitor.h>
+#include <set>
+
+namespace mws {
+
+class DFAInfo;
+
+class CharClassVisitor
+	:
+	public mws::ast::Visitor
+{
+public:
+	virtual void visit(const ast::Negate& n_);
+    virtual void visit(const ast::RngConcat& n_);
+	virtual void visit(const ast::Rng& n_);
+    virtual void visit(const ast::CharClassSymbol& n_);
+
+//protected:
+    std::set<RangeKey> _charClassSet;
+};
+
+}
+
+
